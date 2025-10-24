7a1c5b1d83e4d3a21d1a7deb973db016de364e2f
mtkaz1 <muhammadkazi144@gmail.com>
2025-10-23 19:21:11 -0600

    lesson-5: add state_machine module (c/h), include in main, update CMakeLists

---
+++ app/src/my_state_machine.c
@@ -0,0 +1,20 @@
+/**
+ * @file my_state_machine.c
+ */
+
+ #include <zephyr/smf.h>
+
+ #include "LED.h"
+ #include "my_state_machine.h"
+
+ void state_machine_init() {
+
+
+
+ }
+
+ int state_machine_run() {
+
+
+    
+ }
